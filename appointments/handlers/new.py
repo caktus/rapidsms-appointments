75fcd3cc2ca40e71134f58c3dc08052e6aa2ddf3
Mark Lavin <mlavin@caktusgroup.com>
2012-11-08 09:32:17 -0500

    Starting on test cases for new handler and including help text.

--- appointments/handlers/new.py
+++ appointments/handlers/new.py
@@ -1,5 +1,7 @@
 from __future__ import unicode_literals
 
+from django.utils.translation import ugettext_lazy as _
+
 from .base import AppointmentHandler
 from ..models import Timeline, TimelineSubscription
 
@@ -8,6 +10,11 @@ class NewHandler(AppointmentHandler):
     "Subscribes a user to a timeline."
 
     keyword = 'new'
+
+    def help(self):
+        "Return help mesage."
+        help_text =_('To add a user a timeline send: {0} NEW <NAME/ID> <DATE>. The date is optional.'.format(self.prefix))
+        self.respond(help_text)
 
     def handle(self, text):
         self.respond("Added.")
---
+++ appointments/tests/test_handlers.py
@@ -0,0 +1,19 @@
+from django.test import TestCase
+
+from ..handlers.new import NewHandler
+
+
+class NewHandlerTestCase(TestCase):
+    "Keyword handler for adding users to timelines"
+
+    def test_help(self):
+        "Prefix and keyword should return the help for adding subscriptions."
+        replies = NewHandler.test('APPT NEW')
+        self.assertEqual(len(replies), 1)
+        reply = replies[0]
+        self.assertTrue('APPT NEW <NAME/ID> <DATE>' in reply)
+
+    def test_match(self):
+        "Send a successful match to create user timeline subscription."
+        replies = NewHandler.test('APPT NEW foo')
+        self.assertEqual(len(replies), 1)
