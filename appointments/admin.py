5a9aceba058f402a987d146006eb6d3c0420d6f1
David Ray <davidray@gmail.com>
2013-01-10 08:47:19 -0500

    Adding useful fields for display/filter in Milestone Admin

--- appointments/admin.py
+++ appointments/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from appointments.models import Timeline, TimelineSubscription, Milestone, Appointment, Notification
 
+
+class MilestoneAdmin(admin.ModelAdmin):
+    list_filter = ('timeline',)
+    list_display = ('name', 'timeline', 'offset')
+
 admin.site.register(Timeline)
 admin.site.register(TimelineSubscription)
-admin.site.register(Milestone)
+admin.site.register(Milestone,  MilestoneAdmin)
 admin.site.register(Appointment)
 admin.site.register(Notification)
